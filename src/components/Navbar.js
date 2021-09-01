8c9b2db1e24ecbdaebb58628bfa5c95a9e12ad6e
Victor Garcia Mestre <vgmestre@gmail.com>
2021-09-01 23:28:52 +0200

    added styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import { Flex, Text } from '@chakra-ui/react';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <Flex
+      as="nav"
+      align="center"
+      justify="space-between"
+      wrap="wrap"
+      padding="6"
+      bg="red.400"
+      color="white"
+    >
+      <Text
+        fontFamily="mono"
+        letterSpacing="2px"
+        fontWeight="bold"
+        fontSize="lg"
+      >
+        POKEMON APP
+      </Text>
+    </Flex>
+  );
 };
 
 export default Navbar;
