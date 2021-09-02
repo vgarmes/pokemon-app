b9f643590ff541e69771fa88f045fe4093449743
Victor Garcia Mestre <vgmestre@gmail.com>
2021-09-02 23:40:58 +0200

    fetch and cache all pokemons

--- src/context/global_context.js
+++ src/context/global_context.js
@@ -1,16 +1,62 @@
-import React, { useReducer, useContext } from 'react';
+import React, { useReducer, useContext, useEffect } from 'react';
+import axios from 'axios';
 import reducer from '../reducer/global_reducer';
-import { NEXT_PAGE, PREV_PAGE } from '../actions';
+import {
+  NEXT_PAGE,
+  PREV_PAGE,
+  GET_POKEMONS_BEGIN,
+  GET_POKEMONS_SUCCESS,
+  GET_POKEMONS_ERROR,
+  GET_SINGLE_POKEMON_BEGIN,
+  GET_SINGLE_POKEMON_SUCCESS,
+  GET_SINGLE_POKEMON_ERROR,
+  SORT_POKEMONS,
+} from '../actions';
+import { POKE_LIMIT, pokemons_url } from '../utils/constants';
 
 const initialState = {
   page_size: 20,
   page_index: 0,
+  pokemons_loading: false,
+  pokemons_error: false,
+  single_pokemon_loading: false,
+  single_pokemon_error: false,
+  pokemons: [],
+  sort: 'id',
+  order: 'asc',
 };
 
 const GlobalContext = React.createContext();
 
 export const ContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
+
+  const fetchPokemons = async (url, limit) => {
+    dispatch({ type: GET_POKEMONS_BEGIN });
+    try {
+      const response = await axios.get(`${url}?limit=${limit}`);
+      const pokemons = response.data.results;
+
+      for (let i = 0; i < pokemons.length; i++) {
+        await fetchSinglePokemon(pokemons[i].url);
+      }
+
+      dispatch({ type: GET_POKEMONS_SUCCESS });
+    } catch (error) {
+      dispatch({ type: GET_POKEMONS_ERROR });
+    }
+  };
+
+  const fetchSinglePokemon = async (url) => {
+    dispatch({ type: GET_SINGLE_POKEMON_BEGIN });
+    try {
+      const response = await axios.get(url);
+      const singlePokemon = response.data;
+      dispatch({ type: GET_SINGLE_POKEMON_SUCCESS, payload: singlePokemon });
+    } catch (error) {
+      dispatch({ type: GET_SINGLE_POKEMON_ERROR });
+    }
+  };
 
   const nextPage = () => {
     dispatch({ type: NEXT_PAGE });
@@ -19,6 +65,14 @@ export const ContextProvider = ({ children }) => {
   const prevPage = () => {
     dispatch({ type: PREV_PAGE });
   };
+
+  const sortPokemons = (sort, order) => {
+    dispatch({ type: SORT_POKEMONS, payload: { sort, order } });
+  };
+
+  useEffect(() => {
+    fetchPokemons(pokemons_url, POKE_LIMIT);
+  }, []);
 
   return (
     <GlobalContext.Provider value={{ ...state, nextPage, prevPage }}>
--- src/reducer/global_reducer.js
+++ src/reducer/global_reducer.js
@@ -1,4 +1,14 @@
-import { NEXT_PAGE, PREV_PAGE } from '../actions';
+import {
+  NEXT_PAGE,
+  PREV_PAGE,
+  GET_POKEMONS_BEGIN,
+  GET_POKEMONS_SUCCESS,
+  GET_POKEMONS_ERROR,
+  GET_SINGLE_POKEMON_BEGIN,
+  GET_SINGLE_POKEMON_SUCCESS,
+  GET_SINGLE_POKEMON_ERROR,
+  SORT_POKEMONS,
+} from '../actions';
 
 const global_reducer = (state, action) => {
   if (action.type === NEXT_PAGE) {
@@ -7,6 +17,61 @@ const global_reducer = (state, action) => {
 
   if (action.type === PREV_PAGE) {
     return { ...state, page_index: state.page_index - 1 };
+  }
+
+  if (action.type === GET_POKEMONS_BEGIN) {
+    return { ...state, pokemons_loading: true };
+  }
+
+  if (action.type === GET_POKEMONS_SUCCESS) {
+    return { ...state, pokemons_loading: false };
+  }
+
+  if (action.type === GET_POKEMONS_ERROR) {
+    return { ...state, pokemons_loading: false, pokemons_error: true };
+  }
+
+  if (action.type === GET_SINGLE_POKEMON_BEGIN) {
+    return { ...state, single_pokemon_loading: true };
+  }
+
+  if (action.type === GET_SINGLE_POKEMON_SUCCESS) {
+    const { id, name, weight, height, abilities, sprites } = action.payload;
+    const newPokemon = {
+      id,
+      name,
+      weight,
+      height,
+      abilities: abilities.map(({ ability }) => ability.name),
+      artwork: sprites.other['official-artwork']['front_default'],
+    };
+    return {
+      ...state,
+      single_pokemon_loading: false,
+      pokemons: [...state.pokemons, newPokemon],
+    };
+  }
+
+  if (action.type === GET_SINGLE_POKEMON_ERROR) {
+    return {
+      ...state,
+      single_pokemon_loading: false,
+      single_pokemon_error: true,
+    };
+  }
+
+  if (action.type === SORT_POKEMONS) {
+    const { sort, order } = action.payload;
+    let tempPokemons = [...state.pokemons];
+
+    if (sort === 'id') {
+      if (order === 'asc') {
+        console.log('sorting');
+        tempPokemons.sort((a, b) => a.id - b.id);
+      }
+    }
+
+    return { ...state, pokemons: tempPokemons };
   }
   throw new Error(`No matching "${action.type} - action type`);
 };
