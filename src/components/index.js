1615549735db737fc4d8d7b3890f7ed017ea010d
Victor Garcia Mestre <vgmestre@gmail.com>
2021-09-02 23:43:19 +0200

    created Loading component

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Layout from './Layout';
 import Error from './Error';
+import Loading from './Loading';
 import PokemonCard from './PokemonCard';
 import PageButtons from './PageButtons';
-export { Layout, Error, PokemonCard, PageButtons };
+export { Layout, Error, PokemonCard, PageButtons, Loading };
