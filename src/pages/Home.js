3d39c99b89b17398bf50e0a2343c54bd1ab66785
Victor Garcia Mestre <vgmestre@gmail.com>
2021-09-02 09:30:16 +0200

    data fetching and card display

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { useFetchPokeData } from '../hooks/useFetchPokemons';
+import { SimpleGrid } from '@chakra-ui/react';
+import { Error, PokemonCard } from '../components';
+
+const PAGE_SIZE = 20;
 
 const Home = () => {
-  return <div>Homepage</div>;
+  //const [pageIndex, setPageIndex] = useState(0);
+  const pageIndex = 0;
+  const { data, error } = useFetchPokeData('/pokemon', {
+    limit: PAGE_SIZE,
+    offset: PAGE_SIZE * pageIndex,
+  });
+  return (
+    <SimpleGrid my={[2, null, 6]} minChildWidth="250px" spacing="4">
+      {error && <Error />}
+      {data &&
+        data.results.map((pokemon, index) => (
+          <PokemonCard key={index} {...pokemon} />
+        ))}
+    </SimpleGrid>
+  );
 };
 
 export default Home;
