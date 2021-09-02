d3931b10c21e5365fb3f33aabe19a4803fa22150
Victor Garcia Mestre <vgmestre@gmail.com>
2021-09-02 09:30:45 +0200

    display data

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,7 +1,100 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import {
+  Box,
+  Image,
+  Badge,
+  Tag,
+  StatGroup,
+  Stat,
+  StatLabel,
+  StatNumber,
+} from '@chakra-ui/react';
+import { useFetch } from '../hooks/useFetchPokemons';
 
-const PokemonCard = () => {
-  return <div>PokemonCard</div>;
+const PokemonCard = ({ name, url }) => {
+  const dummy_id = 1;
+  const { data } = useFetch(url);
+
+  return (
+    <>
+      {data && (
+        <Box
+          as={Link}
+          to={`/pokemons/${dummy_id.toString()}`}
+          boxShadow="md"
+          borderWidth="1px"
+          rounded="lg"
+          overflow="hidden"
+          position="relative"
+        >
+          <Image
+            src={data.sprites.other['official-artwork']['front_default']}
+            alt={`${name} sprite`}
+            height="250px"
+            width="100%"
+            objectFit="cover"
+            objectPosition="center"
+          />
+          <Badge position="absolute" top="3" right="3" fontSize="lg">
+            #{data.id}
+          </Badge>
+          <Box p="6">
+            <Box
+              fontWeight="bold"
+              as="h4"
+              lineHeight="tight"
+              textTransform="capitalize"
+              fontSize="xl"
+              isTruncated
+            >
+              {name}
+            </Box>
+
+            <StatGroup mt={1}>
+              <Stat textTransform="uppercase" letterSpacing="wide">
+                <StatLabel fontSize="xs" color="gray.500">
+                  Height
+                </StatLabel>
+                <StatNumber fontSize="md" fontWeight="semibold">
+                  {data.height * 10} cm
+                </StatNumber>
+              </Stat>
+              <Stat textTransform="uppercase" letterSpacing="wide">
+                <StatLabel fontSize="xs" color="gray.500">
+                  Weight
+                </StatLabel>
+                <StatNumber fontSize="md" fontWeight="semibold">
+                  {Math.floor(data.weight / 10)} kg
+                </StatNumber>
+              </Stat>
+            </StatGroup>
+
+            <Box
+              d="flex"
+              mt={1}
+              letterSpacing="wide"
+              textTransform="uppercase"
+              flexWrap="wrap"
+            >
+              {data.abilities.map(({ ability }, index) => (
+                <Tag
+                  key={index}
+                  size="sm"
+                  mr={2}
+                  colorScheme="cyan"
+                  variant="subtle"
+                  mt={1}
+                >
+                  {ability.name}
+                </Tag>
+              ))}
+            </Box>
+          </Box>
+        </Box>
+      )}
+    </>
+  );
 };
 
 export default PokemonCard;
