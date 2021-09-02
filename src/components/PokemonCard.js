5b5720286264790b486e66b93c2bdd49a35c4e70
Victor Garcia Mestre <vgmestre@gmail.com>
2021-09-02 23:44:12 +0200

    modified props according to new pokemon data structure

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -12,88 +12,83 @@ import {
 } from '@chakra-ui/react';
 import { useFetch } from '../hooks/useFetchPokemons';
 
-const PokemonCard = ({ name, url }) => {
+const PokemonCard = ({ id, name, weight, height, abilities, artwork }) => {
   const dummy_id = 1;
-  const { data } = useFetch(url);
 
   return (
-    <>
-      {data && (
+    <Box
+      as={Link}
+      to={`/pokemons/${dummy_id.toString()}`}
+      boxShadow="md"
+      borderWidth="1px"
+      rounded="lg"
+      overflow="hidden"
+      position="relative"
+    >
+      <Image
+        src={artwork}
+        alt={`${name} sprite`}
+        height="300px"
+        width="100%"
+        objectFit="cover"
+        objectPosition="center"
+      />
+      <Badge position="absolute" top="3" right="3" fontSize="lg">
+        #{id}
+      </Badge>
+      <Box p="6">
         <Box
-          as={Link}
-          to={`/pokemons/${dummy_id.toString()}`}
-          boxShadow="md"
-          borderWidth="1px"
-          rounded="lg"
-          overflow="hidden"
-          position="relative"
+          fontWeight="bold"
+          as="h4"
+          lineHeight="tight"
+          textTransform="capitalize"
+          fontSize="xl"
+          isTruncated
         >
-          <Image
-            src={data.sprites.other['official-artwork']['front_default']}
-            alt={`${name} sprite`}
-            height="300px"
-            width="100%"
-            objectFit="cover"
-            objectPosition="center"
-          />
-          <Badge position="absolute" top="3" right="3" fontSize="lg">
-            #{data.id}
-          </Badge>
-          <Box p="6">
-            <Box
-              fontWeight="bold"
-              as="h4"
-              lineHeight="tight"
-              textTransform="capitalize"
-              fontSize="xl"
-              isTruncated
-            >
-              {name}
-            </Box>
-
-            <StatGroup mt={1}>
-              <Stat textTransform="uppercase" letterSpacing="wide">
-                <StatLabel fontSize="xs" color="gray.500">
-                  Height
-                </StatLabel>
-                <StatNumber fontSize="md" fontWeight="semibold">
-                  {data.height * 10} cm
-                </StatNumber>
-              </Stat>
-              <Stat textTransform="uppercase" letterSpacing="wide">
-                <StatLabel fontSize="xs" color="gray.500">
-                  Weight
-                </StatLabel>
-                <StatNumber fontSize="md" fontWeight="semibold">
-                  {Math.floor(data.weight / 10)} kg
-                </StatNumber>
-              </Stat>
-            </StatGroup>
-
-            <Box
-              d="flex"
-              mt={1}
-              letterSpacing="wide"
-              textTransform="uppercase"
-              flexWrap="wrap"
-            >
-              {data.abilities.map(({ ability }, index) => (
-                <Tag
-                  key={index}
-                  size="sm"
-                  mr={2}
-                  colorScheme="cyan"
-                  variant="subtle"
-                  mt={1}
-                >
-                  {ability.name}
-                </Tag>
-              ))}
-            </Box>
-          </Box>
+          {name}
         </Box>
-      )}
-    </>
+
+        <StatGroup mt={1}>
+          <Stat textTransform="uppercase" letterSpacing="wide">
+            <StatLabel fontSize="xs" color="gray.500">
+              Height
+            </StatLabel>
+            <StatNumber fontSize="md" fontWeight="semibold">
+              {height * 10} cm
+            </StatNumber>
+          </Stat>
+          <Stat textTransform="uppercase" letterSpacing="wide">
+            <StatLabel fontSize="xs" color="gray.500">
+              Weight
+            </StatLabel>
+            <StatNumber fontSize="md" fontWeight="semibold">
+              {Math.floor(weight / 10)} kg
+            </StatNumber>
+          </Stat>
+        </StatGroup>
+
+        <Box
+          d="flex"
+          mt={1}
+          letterSpacing="wide"
+          textTransform="uppercase"
+          flexWrap="wrap"
+        >
+          {abilities.map((ability, index) => (
+            <Tag
+              key={index}
+              size="sm"
+              mr={2}
+              colorScheme="cyan"
+              variant="subtle"
+              mt={1}
+            >
+              {ability}
+            </Tag>
+          ))}
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,30 +1,37 @@
 import React from 'react';
-import { useFetchPokeData } from '../hooks/useFetchPokemons';
+import { useFetchOptions } from '../hooks/useFetchPokemons';
 import { SimpleGrid } from '@chakra-ui/react';
-import { Error, PokemonCard, PageButtons } from '../components';
+import { Error, Loading, PokemonCard, PageButtons } from '../components';
 import { useGlobalContext } from '../context/global_context';
+import { POKE_LIMIT } from '../utils/constants';
 
 const Home = () => {
-  const { page_size, page_index } = useGlobalContext();
+  const { pokemons, pokemons_loading, pokemons_error } = useGlobalContext();
 
-  const { data, error, isValidating } = useFetchPokeData('/pokemon', {
-    limit: page_size,
-    offset: page_size * page_index,
-  });
-  if (error) {
+  if (pokemons_loading) {
+    return <Loading />;
+  }
+  if (pokemons_error) {
     return <Error />;
   }
+
+  const display_pokemons = pokemons.slice(0, 19);
   return (
-    <>
-      <PageButtons isLoading={isValidating} isLastPage={data && !data.next} />
-      <SimpleGrid my={[2, null, 6]} minChildWidth="300px" spacing="4">
-        {data &&
+    <SimpleGrid my={[2, null, 6]} minChildWidth="300px" spacing="4">
+      {display_pokemons.length !== 0 &&
+        display_pokemons.map((pokemon) => (
+          <PokemonCard key={pokemon.id} {...pokemon} />
+        ))}
+    </SimpleGrid>
+  );
+};
+/*
+ <SimpleGrid my={[2, null, 6]} minChildWidth="300px" spacing="4">
+{data &&
           data.results.map((pokemon, index) => (
             <PokemonCard key={index} {...pokemon} />
           ))}
-      </SimpleGrid>
-    </>
-  );
-};
+          </SimpleGrid>
+          */
 
 export default Home;
