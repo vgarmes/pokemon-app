b446161854e2a5af4b3ef90d1bfdee6bee5c9f65
Victor Garcia Mestre <vgmestre@gmail.com>
2021-09-03 23:28:39 +0200

    removed unnecessary usestate import

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { FormControl, FormLabel, Select } from '@chakra-ui/react';
 import { useGlobalContext } from '../context/global_context';
 
