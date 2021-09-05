ebc8aa1746336c5de3ef24b440462938f1ca5862
Victor Garcia Mestre <vgmestre@gmail.com>
2021-09-05 17:58:57 +0200

    removed form element

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -6,36 +6,34 @@ const Sort = () => {
   const { sort, updateSort } = useGlobalContext();
 
   return (
-    <form onSubmit={(e) => e.preventDefault}>
-      <FormControl display="flex" alignItems="center">
-        <FormLabel
-          htmlFor="sort"
-          flexShrink="0"
-          p="0"
-          mr={2}
-          fontSize="sm"
-          display={['none', 'block']}
-        >
-          Sort by
-        </FormLabel>
-        <Select
-          variant="outline"
-          size="sm"
-          name="sort"
-          id="sort"
-          fontSize="sm"
-          value={sort}
-          onChange={(e) => updateSort(e.target.value)}
-        >
-          <option value="name-a" data-sort="name" data-order="desc">
-            name (a-z)
-          </option>
-          <option value="name-z" data-sort="name" data-order="asc">
-            name (z-a)
-          </option>
-        </Select>
-      </FormControl>
-    </form>
+    <FormControl display="flex" alignItems="center">
+      <FormLabel
+        htmlFor="sort"
+        flexShrink="0"
+        p="0"
+        mr={2}
+        fontSize="sm"
+        display={['none', 'block']}
+      >
+        Sort by
+      </FormLabel>
+      <Select
+        variant="outline"
+        size="sm"
+        name="sort"
+        id="sort"
+        fontSize="sm"
+        value={sort}
+        onChange={(e) => updateSort(e.target.value)}
+      >
+        <option value="name-a">name (a-z)</option>
+        <option value="name-z">name (z-a)</option>
+        <option value="height-highest">height (highest)</option>
+        <option value="height-lowest">height (lowest)</option>
+        <option value="weight-highest">weight (highest)</option>
+        <option value="weight-lowest">weight (lowest)</option>
+      </Select>
+    </FormControl>
   );
 };
 
