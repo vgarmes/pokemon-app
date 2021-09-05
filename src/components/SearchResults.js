2f05f52e8bae614289d48c50fe28916132665e66
Victor Garcia Mestre <vgmestre@gmail.com>
2021-09-05 21:02:26 +0200

    show error if error

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -2,13 +2,12 @@ import React from 'react';
 import { useFetch, useFetchUrl } from '../hooks/useFetchPokemons';
 import { useGlobalContext } from '../context/global_context';
 import { searchFilter } from '../utils';
-import { PokemonCard } from './';
+import { PokemonCard, Error } from './';
 import { SimpleGrid } from '@chakra-ui/react';
 
 const SearchResults = () => {
-  const { search_term, search_category, page_size, page_index } =
-    useGlobalContext();
-  const { data, error, isValidating } = useFetch(`/${search_category}`, {
+  const { search_term, search_category } = useGlobalContext();
+  const { data, error } = useFetch(`/${search_category}`, {
     limit: 2000,
   });
 
@@ -24,6 +23,7 @@ const SearchResults = () => {
   if (search_category === 'ability') {
     return (
       <SimpleGrid my={[2, null, 6]} columns={[1, 2, 3]} spacing="4">
+        {error && <Error />}
         {data &&
           results.map((result, index) => (
             <CategoryWrapper key={index} {...result} />
@@ -34,6 +34,7 @@ const SearchResults = () => {
 
   return (
     <SimpleGrid my={[2, null, 6]} columns={[1, 2, 3]} spacing="4">
+      {error && <Error />}
       {data &&
         results.map((result, index) => <PokemonCard key={index} {...result} />)}
     </SimpleGrid>
@@ -44,6 +45,7 @@ const CategoryWrapper = ({ url }) => {
   const { data, error } = useFetchUrl(url);
   return (
     <>
+      {error && <Error />}
       {data &&
         data.pokemon.map(({ pokemon }, index) => (
           <PokemonCard key={index} {...pokemon} />
