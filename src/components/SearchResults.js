4190688900bf3692eefcc99ed4c94b80c91edb39
Victor Garcia Mestre <vgmestre@gmail.com>
2021-09-05 15:06:27 +0200

    finished search functionality

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,7 +1,41 @@
 import React from 'react';
+import { useFetch, useFetchUrl } from '../hooks/useFetchPokemons';
+import { useGlobalContext } from '../context/global_context';
+import { searchFilter } from '../utils';
+import { PokemonCard } from './';
+import { SimpleGrid } from '@chakra-ui/react';
 
 const SearchResults = () => {
-  return <div>Search results</div>;
+  const { search_term, search_category } = useGlobalContext();
+  const { data, error, isValidating } = useFetch(`/${search_category}`, {
+    limit: 2000,
+  });
+
+  return (
+    <SimpleGrid my={[2, null, 6]} columns={[1, 2, 3]} spacing="4">
+      {data &&
+        searchFilter(data.results, { name: search_term }).map((result, index) =>
+          search_category === 'ability' ? (
+            <CategoryWrapper key={index} {...result} />
+          ) : (
+            <PokemonCard key={index} {...result} />
+          )
+        )}
+    </SimpleGrid>
+  );
+};
+
+const CategoryWrapper = ({ url }) => {
+  const { data, error } = useFetchUrl(url);
+  console.log(data);
+  return (
+    <>
+      {data &&
+        data.pokemon.map(({ pokemon }, index) => (
+          <PokemonCard key={index} {...pokemon} />
+        ))}
+    </>
+  );
 };
 
 export default SearchResults;
