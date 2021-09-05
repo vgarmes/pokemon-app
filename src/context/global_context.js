f28cd91f9fb1f594bac4a3fd26845cc3c8d6a14f
Victor Garcia Mestre <vgmestre@gmail.com>
2021-09-05 17:57:53 +0200

    cleanup unused functions

--- src/context/global_context.js
+++ src/context/global_context.js
@@ -4,28 +4,15 @@ import reducer from '../reducer/global_reducer';
 import {
   NEXT_PAGE,
   PREV_PAGE,
-  GET_POKEMONS_BEGIN,
-  GET_POKEMONS_SUCCESS,
-  GET_POKEMONS_ERROR,
-  GET_SINGLE_POKEMON_BEGIN,
-  GET_SINGLE_POKEMON_SUCCESS,
-  GET_SINGLE_POKEMON_ERROR,
   UPDATE_SORT,
-  SORT_POKEMONS,
   UPDATE_PAGE_SIZE,
   UPDATE_SEARCH_TERM,
   UPDATE_SEARCH_CATEGORY,
 } from '../actions';
-import { POKE_LIMIT, pokemons_url } from '../utils/constants';
 
 const initialState = {
   page_size: 20,
   page_index: 0,
-  pokemons_loading: false,
-  pokemons_error: false,
-  single_pokemon_loading: false,
-  single_pokemon_error: false,
-  pokemons: [],
   sort: 'name-a',
   search_term: '',
   search_category: 'pokemon',
@@ -35,33 +22,6 @@ const GlobalContext = React.createContext();
 
 export const ContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
-
-  const fetchPokemons = async (url, limit) => {
-    dispatch({ type: GET_POKEMONS_BEGIN });
-    try {
-      const response = await axios.get(`${url}?limit=${limit}`);
-      const pokemons = response.data.results;
-
-      for (let i = 0; i < pokemons.length; i++) {
-        await fetchSinglePokemon(pokemons[i].url);
-      }
-
-      dispatch({ type: GET_POKEMONS_SUCCESS });
-    } catch (error) {
-      dispatch({ type: GET_POKEMONS_ERROR });
-    }
-  };
-
-  const fetchSinglePokemon = async (url) => {
-    dispatch({ type: GET_SINGLE_POKEMON_BEGIN });
-    try {
-      const response = await axios.get(url);
-      const singlePokemon = response.data;
-      dispatch({ type: GET_SINGLE_POKEMON_SUCCESS, payload: singlePokemon });
-    } catch (error) {
-      dispatch({ type: GET_SINGLE_POKEMON_ERROR });
-    }
-  };
 
   const increasePage = () => {
     dispatch({ type: NEXT_PAGE });
--- src/reducer/global_reducer.js
+++ src/reducer/global_reducer.js
@@ -1,14 +1,7 @@
 import {
   NEXT_PAGE,
   PREV_PAGE,
-  GET_POKEMONS_BEGIN,
-  GET_POKEMONS_SUCCESS,
-  GET_POKEMONS_ERROR,
-  GET_SINGLE_POKEMON_BEGIN,
-  GET_SINGLE_POKEMON_SUCCESS,
-  GET_SINGLE_POKEMON_ERROR,
   UPDATE_SORT,
-  SORT_POKEMONS,
   UPDATE_PAGE_SIZE,
   UPDATE_SEARCH_TERM,
   UPDATE_SEARCH_CATEGORY,
@@ -23,68 +16,8 @@ const global_reducer = (state, action) => {
     return { ...state, page_index: state.page_index - 1 };
   }
 
-  if (action.type === GET_POKEMONS_BEGIN) {
-    return { ...state, pokemons_loading: true };
-  }
-
-  if (action.type === GET_POKEMONS_SUCCESS) {
-    return { ...state, pokemons_loading: false };
-  }
-
-  if (action.type === GET_POKEMONS_ERROR) {
-    return { ...state, pokemons_loading: false, pokemons_error: true };
-  }
-
-  if (action.type === GET_SINGLE_POKEMON_BEGIN) {
-    return { ...state, single_pokemon_loading: true };
-  }
-
-  if (action.type === GET_SINGLE_POKEMON_SUCCESS) {
-    const { id, name, weight, height, abilities, sprites } = action.payload;
-    const newPokemon = {
-      id,
-      name,
-      weight,
-      height,
-      abilities: abilities.map(({ ability }) => ability.name),
-      artwork: sprites.other['official-artwork']['front_default'],
-    };
-    return {
-      ...state,
-      single_pokemon_loading: false,
-      pokemons: [...state.pokemons, newPokemon],
-    };
-  }
-
-  if (action.type === GET_SINGLE_POKEMON_ERROR) {
-    return {
-      ...state,
-      single_pokemon_loading: false,
-      single_pokemon_error: true,
-    };
-  }
-
   if (action.type === UPDATE_SORT) {
     return { ...state, sort: action.payload };
-  }
-
-  if (action.type === SORT_POKEMONS) {
-    const { sort } = state;
-    let tempPokemons = [...state.pokemons];
-    console.log('sorting');
-
-    if (sort === 'name-a') {
-      tempPokemons.sort((a, b) => {
-        return a.name.localeCompare(b.name);
-      });
-    }
-    if (sort === 'name-z') {
-      tempPokemons.sort((a, b) => {
-        return b.name.localeCompare(a.name);
-      });
-    }
-
-    return { ...state, pokemons: tempPokemons };
   }
 
   if (action.type === UPDATE_PAGE_SIZE) {
@@ -98,6 +31,7 @@ const global_reducer = (state, action) => {
   if (action.type === UPDATE_SEARCH_CATEGORY) {
     return { ...state, search_category: action.payload };
   }
+
   throw new Error(`No matching "${action.type} - action type`);
 };
 
