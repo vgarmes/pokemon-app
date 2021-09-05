93ad32fbb32f6170b862a9e74bb65ab52a6a1438
Victor Garcia Mestre <vgmestre@gmail.com>
2021-09-05 18:57:10 +0200

    show no results message

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -6,21 +6,36 @@ import { PokemonCard } from './';
 import { SimpleGrid } from '@chakra-ui/react';
 
 const SearchResults = () => {
-  const { search_term, search_category } = useGlobalContext();
+  const { search_term, search_category, page_size, page_index } =
+    useGlobalContext();
   const { data, error, isValidating } = useFetch(`/${search_category}`, {
     limit: 2000,
   });
 
+  let results = [];
+  if (data) {
+    results = searchFilter(data.results, { name: search_term });
+  }
+
+  if (results.length === 0) {
+    return <div>No results found</div>;
+  }
+
+  if (search_category === 'ability') {
+    return (
+      <SimpleGrid my={[2, null, 6]} columns={[1, 2, 3]} spacing="4">
+        {data &&
+          results.map((result, index) => (
+            <CategoryWrapper key={index} {...result} />
+          ))}
+      </SimpleGrid>
+    );
+  }
+
   return (
     <SimpleGrid my={[2, null, 6]} columns={[1, 2, 3]} spacing="4">
       {data &&
-        searchFilter(data.results, { name: search_term }).map((result, index) =>
-          search_category === 'ability' ? (
-            <CategoryWrapper key={index} {...result} />
-          ) : (
-            <PokemonCard key={index} {...result} />
-          )
-        )}
+        results.map((result, index) => <PokemonCard key={index} {...result} />)}
     </SimpleGrid>
   );
 };
